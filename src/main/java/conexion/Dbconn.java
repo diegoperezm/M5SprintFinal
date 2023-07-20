e01f83a8dc087a3e0186426eb82ca287ad1cdbb4
diegoperezm <diegoperezcorreo@gmail.com>
2023-07-20 11:42:53 -0400

    Limpiar comentarios

--- src/main/java/conexion/Dbconn.java
+++ src/main/java/conexion/Dbconn.java
@@ -21,10 +21,3 @@ public class Dbconn {
       return connection;
 	}
 }
-
-/*	
-	public static String url = "jdbc:mysql://localhost:3306/prevencion_riesgos";
-	public static String user = "usuario";
-	public static String password = "usuario";
-	public static String driver = "com.mysql.cj.jdbc.Driver";
-	*/
